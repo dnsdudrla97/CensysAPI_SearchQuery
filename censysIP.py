8653adcaf0f630ce4c26708ae2f9d5dc8a79e68d
root <root@localhost.localdomain>
2020-05-20 08:21:00 -0400

    formmat

--- censysIP.py
+++ censysIP.py
@@ -4,40 +4,9 @@ import pickle
 import argparse
 import time
 import sys
-import re
 import os
 
-# API = "API"
-# SECRET = "SECRET"
-# c = censys.ipv4.CensysIPv4(api_id=API, api_secret=SECRET)
-
-# Report method Query
-# c.report(""" "welcome to" AND tags.raw: "http" """, field="80.http.get.headers.server.raw", buckets=5)
-
-
-# View method IP ALLL -> JSON FIlE
-# ip = c.view('8.8.8.8')
-# print(type(ip))
-
-
-# search method -> Index filed, all text, combine
-# for result in c.search("80.http.get.headers.server: Apache AND location.country: Japan", max_records=10):
-#     print(result)
-
-# custome filed 80, 443, 25, 110
-# IPV4_FIELDS = ['ip',
-# 		 'updated_at',
-# 		 '80.http.get.title',
-# 		 '443.https.get.title',
-# 		 '443.https.tls.certificate.parsed.subject_dn',
-# 		 '443.https.tls.certificate.parsed.names',
-# 		 '443.https.tls.certificate.parsed.subject.common_name',
-# 		 '443.https.tls.certificate.parsed.extensions.subject_alt_name.dns_names',
-# 		 '25.smtp.starttls.tls.certificate.parsed.names',
-# 		 '25.smtp.starttls.tls.certificate.parsed.subject_dn',
-# 		 '110.pop3.starttls.tls.certificate.parsed.names',
-# 		 '110.pop3.starttls.tls.certificate.parsed.subject_dn']
-
+# 필터링 데이터 구조
 filter_fields = ['location.country', 'location.country_code', 'location.city', 'ip',
                  'protocols', 'autonomous_system.name',
                  'autonomous_system.asn',
@@ -51,25 +20,7 @@ filter_fields = ['location.country', 'location.country_code', 'location.city', '
                  '80.http.get.body',
                  'metadata.os', 'tags']
 
-
-
-# def searchQuery(server, country, maxrecords):
-# 	queryOptions(server, country, maxrecords)
-# 	data = list(c.search(f"80.http.get.headers.server: {server} AND location.country: {country}", IPV4_FIELDS, max_records=maxrecords))
-# 	queryFilter(data)
-
-# def queryOptions(server, country, maxrecords):
-# 	print("server: {}".format(server))
-# 	print("country: {}".format(country))
-# 	print("maxrecords {}".format(maxrecords))
-# 	print(f"80.http.get.headers.server:{server} AND location.country:{country} max_records={maxrecords}")
-
-# def queryFilter(data):
-# 	print("{:<15}{:^30}{:>30}".format('IP', 'UPDATE', 'TITLE'))
-# 	for d in data:
-# 		print("{ip:<30} {update:^30}".format(ip=d['ip'], update=d['updated_at']))
-# 		# print(f'{ip:10} ==> {update:10}')
-# query_string
+# 인자를 바탕으로 쿼리문을 제작합니다. 
 def build_query_string(args):
 	if len(args.arguments) == 0:
 		s = '*'		
@@ -81,22 +32,19 @@ def build_query_string(args):
 			tags_q = " AND tags:" + " AND tags:".join(tags_l)
 		else:
 			tags_q = " AND tags: %s" % args.tags
-		s += tags_q
-		# print(s)
+		s += tags_q		
 	if args.asn:
 		s += " AND autonomous_system.asn: %s" % args.asn
 	if args.country:
 		s += " AND location.country_code:%s" % args.country
 	if args.http_server:
-		s += " AND 80.http.get.headers.server:%s" % args.http_server
-	
+		s += " AND 80.http.get.headers.server:%s" % args.http_server	
 	if args.html_title:
 		if " " in args.html_title:
 			title = "\"%s\"" % args.html_title
 		else:
 			title = args.html_title
-		s += " AND 80.http.get.title:%s" % title
-	print(s)
+		s += " AND 80.http.get.title:%s" % title	
 	if args.html_body:
 		if " " in args.html_body:
 			body = "\"%s\"" % args.html_body
@@ -105,9 +53,7 @@ def build_query_string(args):
 		s += " AND 80.http.get.body:%s" % body
 	return s
 
-# res = complete dict from IPv4 search with generic info
-
-
+# res, ipv4 기반의 일반 정보를 검색 -> dict 형태
 def print_short(res):
 	max_title_len = 50
 	title_head = 'Title: '
@@ -121,23 +67,10 @@ def print_short(res):
 	tags = res.get('tags', '')
 	ip = res.get('ip', 'N/A')
 
-
-	# print(max_title_len, title_head, cut, http_title,
-    #   as_name, as_num, loc, os, tags, ip)
-
 	http_title = http_title.replace('\n', '\\n')
 	http_title = http_title.replace('\r', '\\r')
 
-    # do some destructive encoding to UTF-8
-	# http_title = http_title.encode('UTF-8', errors='ignore')
-	# tags = b', '.join([t.encode('UTF-8', errors='ignore') for t in tags])
-	# as_name = as_name.encode('UTF-8', errors='ignore')
-	# os = os.encode('UTF-8', errors='ignore')
-	# loc = loc.decode('ASCII', errors='ignore')
-
-    # shortun title if too long
-	# print(len(http_title))
-	# print(max_title_len-len(title_head)-len(cut)-1)
+    # 타이틀이 길 경우 길이 제한
 	if len(http_title) > (max_title_len-len(title_head)-1):
 		http_title = str(http_title[:max_title_len-len(title_head)-len(cut)-1]) + cut
 
@@ -148,34 +81,18 @@ def print_short(res):
 			('OS: %s' % os).ljust(15) +
 			('Tags: %s' % tags))
 
-
-
-# def main():
-# 	parser = argparse.ArgumentParser()
-#     # name argument 추가
-# 	parser.add_argument('server')
-# 	parser.add_argument('country')
-# 	parser.add_argument('maxrecords', type=int)
-# 	args = parser.parse_args()
-
-# 	searchQuery(server = args.server,
-# 		country = args.country,
-# 		maxrecords = args.maxrecords
-# 	)
-
-# 	# queryOptions(server, country, maxrecords)
-
+# help 설명문
 help_desc = '''
-Censys query via command line
+Censys API를 활용한 OSINT
 -- Younsle
 '''
 
-
+# API 설정 - 환경 변수 파일 생성
 def conf_get_censys_api(args):
 	conf_file = "%s/.censys_API.p" % os.environ.get('HOME')
 	api = dict()
 
-    # command-line API key get precedence other methods
+    # 인자가 있을 경우 해당 환경 변수 파일 생성 및 저장
 	if args.api_id and args.api_secret:
 		api['id'] = args.api_id
 		api['secret'] = args.api_secret
@@ -194,56 +111,56 @@ def conf_get_censys_api(args):
 		try:
 			api = pickle.load(open(conf_file, "rb"))
 		except:
-			print ("Pickle file corrupted.")
+			print ("피클 파일이 손상되었습니다.")
 			sys.exit(-1)
 		if not api.get('id') or not api.get('secret'):
-			print ("Pickle file structure mismatch.")
+			print ("피크 파일 구조가 불일치 합니다.")
 			sys.exit(-2)
 		return api
 
-    # if environment variable exists, store it in file
+    # 환경 변수가 존재 하지 않을시 저장
 	if 'CENSYS_API_ID' in os.environ and 'CENSYS_API_SECRET' in os.environ:
 		api['id'] = os.environ.get('CENSYS_API_ID')
 		api['secret'] = os.environ.get('CENSYS_API_SECRET')
 		pickle.dump(api, open(conf_file, "wb"))
 		return api
 
-
+# 해당 소스가 현재 main 에서 동작하는지
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		description=help_desc, formatter_class=argparse.RawTextHelpFormatter)
 
-	# query filter shortcuts
+	# 단순 검색 인자 
 	parser.add_argument('-a', '--asn', default=None,
-	                    help='Filter with ASN (ex: 25408 for Westcall-SPB AS)')
+	                    help='기관이,단체가 독립적으로 운영하는 네트워크 라우터를 입력해 주세요')
 	parser.add_argument('-c', '--country', default=None,
-	                    help='Filter with country')
-	parser.add_argument('-S', '--http-server', default=None, help='Server header')
+	                    help='국가 코드를 입력해 주세요')
+	parser.add_argument('-S', '--http-server', default=None, help='서버 <header> 정보를 입력해 주세요')
 	parser.add_argument('-t', '--html-title', default=None,
-	                    help='Filter on html page title')
+	                    help='웹 페이지 <title> 정보를 입력해 주세요')
 	parser.add_argument('-b', '--html-body', default=None,
-	                    help='Filter on html body content')
+	                    help='웹 페이지 <body> 정보를 입력해 주세요')
 	parser.add_argument('-T', '--tags', default=None,
-	                    help='Filter on specific tags. E.g: -T tag1,tag2,... (use keyword \'list\' to list usual tags')
+	                    help='검색 하고자 하는 tag를 입력해 주세요 (-T tag1,tag2,... ) (키워드 사용시 \'list\' 사용해야 정확히 나옵니다.')
 
 	parser.add_argument('--api_id', default=None,
-	                    help='Censys API ID (optional if no env defined)')
+	                    help='환경변수로 정의 하지 않을시 입력해 주세요 (API_ID)')
 	parser.add_argument('--api_secret', default=None,
-	                    help='Censys API SECRET (optional if no env defined)')
+	                    help='환경변수로 정의 하지 않을시 입력해 주세요 (API_SECRET)')
 	parser.add_argument('arguments', metavar='arguments',
-	                    nargs='*', help='Censys query')
+	                    nargs='*', help='쿼리 나열')
 	args = parser.parse_args()
 
-	# handle API key/secret
+	# API 키, 비밀키를 처리 합니다.
 	api = conf_get_censys_api(args)
 	# print(api)
 
-	# build up query
+	# API 인증을 진행 합니다.
 	q = CensysIPv4(api_id=api['id'], api_secret=api['secret'])
-	
+	# 인자를 받아와 분류 후 쿼리문을 만듭니다.
 	s = build_query_string(args)
 	
-
+	# 정리한 쿼리를 바탕으로 search 함수를 사용하여 검색을 진행합니다. (필터 리스트 를 사용하여 찾고자 하는 정보를 분산시킵니다.)
 	r = q.search(s, fields=filter_fields)
 	for e in r:
 		print_short(e)
